1b8ca956c4899ef342e269fff1b9b335f0a5f67e
badiwidya <badiwidya@users.noreply.github.com>
2024-12-05 12:17:11 +0700

    Solve exercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
+  const startPoint = (a < b) ? a : b;
+  const endPoint = (a < b) ? b : a;
+  let sum = 0;
+  for (let i = startPoint; i <= endPoint; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
