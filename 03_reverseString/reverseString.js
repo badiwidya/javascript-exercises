7f8e1a42299979683804d74175748536b741c93f
badiwidya <badiwidya@users.noreply.github.com>
2024-12-05 10:10:32 +0700

    Solve exercise three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+  let textLen = text.length;
+  const splittedText = text.split("");
+  let resultString = "";
+  while (textLen) {
+    resultString += splittedText[textLen - 1];
+    textLen--;
+  }
+  return resultString;
 };
 
 // Do not edit below this line
