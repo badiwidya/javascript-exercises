3cfe1e767b502301268ceb83670713c7fbcbd585
badiwidya <badiwidya@users.noreply.github.com>
2024-12-05 09:38:47 +0700

    Solve exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+  let stringResult = "";
+  if (num < 0) return "ERROR";
+  while (num--) {
+    stringResult += text;
+  }
+  return stringResult;
 };
 
 // Do not edit below this line
