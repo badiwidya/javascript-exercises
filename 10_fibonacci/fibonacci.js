047abe70461c65b8ba43763a741f1515bd1951a9
badiwidya <adiwidya.berton13@gmail.com>
2024-12-06 18:02:25 +0000

    Solve exercise ten

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  const seq = Number(num);
+  if (seq === 1) {
+    return 1;
+  } else if (seq === 0) {
+    return 0;
+  }
+  return fibonacci(seq-1) + fibonacci(seq-2);
 };
 
 // Do not edit below this line
