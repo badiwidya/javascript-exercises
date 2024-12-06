49a8ff4277ae5d8815d3da4b846d22fa0d12652f
badiwidya <adiwidya.berton13@gmail.com>
2024-12-06 17:47:24 +0000

    Solve exercise nine

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const regex1 = /[a-zA-Z0-9]/
+  const regex2 = /[^a-zA-Z0-9]/g
+  let palindrome = string.toLowerCase().split('').filter((s) => {
+    return s.match(regex1);
+  });
+  const sanitizedString = string.replace(regex2, '').toLowerCase();
+  const reversedString = palindrome.reverse().join('')
+  if (sanitizedString === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(palindromes('A car, a man, a maraca.'))
 
 // Do not edit below this line
 module.exports = palindromes;
