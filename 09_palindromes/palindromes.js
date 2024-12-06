617344d65af672208f32db77a95faa20c67faf74
badiwidya <adiwidya.berton13@gmail.com>
2024-12-06 17:52:56 +0000

    Format the document

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,14 @@
 const palindromes = function (string) {
-  const regex1 = /[a-zA-Z0-9]/
-  const regex2 = /[^a-zA-Z0-9]/g
-  let palindrome = string.toLowerCase().split('').filter((s) => {
-    return s.match(regex1);
-  });
-  const sanitizedString = string.replace(regex2, '').toLowerCase();
-  const reversedString = palindrome.reverse().join('')
+  const regex1 = /[a-zA-Z0-9]/;
+  const regex2 = /[^a-zA-Z0-9]/g;
+  let palindrome = string
+    .toLowerCase()
+    .split("")
+    .filter((s) => {
+      return s.match(regex1);
+    });
+  const sanitizedString = string.replace(regex2, "").toLowerCase();
+  const reversedString = palindrome.reverse().join("");
   if (sanitizedString === reversedString) {
     return true;
   } else {
@@ -13,7 +16,7 @@ const palindromes = function (string) {
   }
 };
 
-console.log(palindromes('A car, a man, a maraca.'))
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
