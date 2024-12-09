07bf95d88924caea331e2a273e857b7b6938d5fd
badiwidya <adiwidya.berton13@gmail.com>
2024-12-09 11:11:15 +0700

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(datas) {
+  return datas.reduce((prev, current) => {
+    let prevAge;
+    let currentAge;
+    const yearNow = new Date().getFullYear();
+    if (!current.yearOfDeath) {
+      currentAge = yearNow - current.yearOfBirth;
+      prevAge = prev.yearOfDeath - prev.yearOfBirth;
+    } else if (!prev.yearOfDeath) {
+      prevAge = yearNow - prev.yearOfBirth;
+      currentAge = current.yearOfDeath - current.yearOfBirth;
+    } else {
+      currentAge = current.yearOfDeath - current.yearOfBirth;
+      prevAge = prev.yearOfDeath - prev.yearOfBirth;
+    }
+    return (currentAge > prevAge) ? current : prev;
+  });
 };
 
 // Do not edit below this line
